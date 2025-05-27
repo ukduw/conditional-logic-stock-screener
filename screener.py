460ee58e04704baebc7e6b5d8a94cf1a0baade3f
ukduw <edhkmayo@gmail.com>
2025-05-27 12:00:06 +0100

    removed relative volume from screener, updated requirements for scrape method

--- screener.py
+++ screener.py
@@ -7,9 +7,8 @@ def passes_custom_filter(ticker):
         float_shares = info.get("floatShares")
         dollar_volume = info.get("regularMarketPrice") * volume
         market_cap = info.get("marketCap")
-        # avg_volume_30d = info.get("averageVolume")
 
-        # 1) Float rotation check, 2) $Vol > MCap, UPDATE: REMOVED "3) Relative volume"
+        # 1) Float rotation check, 2) $Vol > MCap"
         return volume > float_shares and dollar_volume > market_cap
     except Exception as e:
         print(f"Error for {ticker}: {e}")

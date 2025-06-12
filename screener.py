ca4a5006ec20cff756b3956438f2f1c6caddf495
ukduw <edhkmayo@gmail.com>
2025-06-12 19:43:58 -0400

    conditions changed from AND to OR - yf data availability caused screener to miss hits... - regularMarketPrice replaced by currentPrice? yf error 401, possibly related to major outage...

--- screener.py
+++ screener.py
@@ -5,11 +5,11 @@ def passes_custom_filter(ticker):
         info = yf.Ticker(ticker).info
         volume = info.get("volume")
         float_shares = info.get("floatShares")
-        dollar_volume = info.get("regularMarketPrice") * volume
+        dollar_volume = info.get("currentPrice") * volume # regularMarketPrice?
         market_cap = info.get("marketCap")
 
         # 1) Float rotation check, 2) $Vol > MCap", 3) 
-        return volume > float_shares and dollar_volume > market_cap
+        return volume > float_shares or dollar_volume > market_cap
     except Exception as e:
         print(f"Error for {ticker}: {e}")
         return False

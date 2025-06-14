df97ef51401371bf085b70c231f01be838eb073a
ukduw <edhkmayo@gmail.com>
2025-06-14 07:42:53 -0400

    removed redundant part of return statement

--- screener.py
+++ screener.py
@@ -9,7 +9,7 @@ def passes_custom_filter(ticker):
         market_cap = info.get("marketCap")
 
         # 1) Float rotation check, 2) $Vol > MCap", 3) 
-        return (volume > float_shares and dollar_volume > market_cap) if float_shares is not None else (volume > float_shares or dollar_volume > market_cap)
+        return (volume > float_shares and dollar_volume > market_cap) if float_shares is not None else (dollar_volume > market_cap)
     except Exception as e:
         print(f"Error for {ticker}: {e}")
         return False

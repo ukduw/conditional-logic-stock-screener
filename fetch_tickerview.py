58b54b13b0500c2d25ac68c6d763a1f14b11c0df
ukduw <edhkmayo@gmail.com>
2025-06-28 05:22:36 -0400

    removed test line

--- fetch_tickerview.py
+++ fetch_tickerview.py
@@ -20,4 +20,3 @@ def get_finviz_tickers_from_tickerview(filter_url):
     
     return tickers
 
-get_finviz_tickers_from_tickerview('https://finviz.com/screener.ashx?v=411&f=cap_smallunder%2Cgeo_usa%2Csh_curvol_o2000%2Csh_float_u50%2Csh_price_u20%2Csh_relvol_o3&o=-change')

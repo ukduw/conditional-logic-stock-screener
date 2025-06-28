60ec048a1dc04964509dedebbbb382346e8e54f1
ukduw <edhkmayo@gmail.com>
2025-06-28 05:24:59 -0400

    rewritten for new scrape module, commented out old one + different screener URLs - remember to change file path back to absolute path for cron

--- run.py
+++ run.py
@@ -1,11 +1,22 @@
 from fetch_tickers import get_all_finviz_tickers
+from fetch_tickerview import get_finviz_tickers_from_tickerview
 from screener import filtered_tickers
 import csv
 import datetime, calendar
 
-base_url = "https://finviz.com/screener.ashx?v=111&f=cap_smallunder%2Cgeo_usa%2Csh_curvol_o1000%2Csh_price_u20%2Csh_relvol_o2&o=-change"
+base_url = "https://finviz.com/screener.ashx?v=411&f=cap_smallunder%2Cgeo_usa%2Csh_curvol_o2000%2Csh_float_u50%2Csh_price_u20%2Csh_relvol_o3&o=-change"
+    # initial screener
+    # https://finviz.com/screener.ashx?v=111&f=cap_smallunder%2Cgeo_usa%2Csh_curvol_o1000%2Csh_price_u20%2Csh_relvol_o2&o=-change
 
-tickers = get_all_finviz_tickers(base_url)
+    # more stringent screener
+        # (r.vol 3x, float <50mil (change??), intra vol >2mil)
+    # https://finviz.com/screener.ashx?v=111&f=cap_smallunder,geo_usa,sh_curvol_o2000,sh_float_u50,sh_price_u20,sh_relvol_o3&o=-change
+
+    # more stringent, TICKER ONLY (must use fetch_tickerview)
+    # https://finviz.com/screener.ashx?v=411&f=cap_smallunder%2Cgeo_usa%2Csh_curvol_o2000%2Csh_float_u50%2Csh_price_u20%2Csh_relvol_o3&o=-change
+
+# tickers = get_all_finviz_tickers(base_url)
+tickers = get_finviz_tickers_from_tickerview(base_url)
 filtered = filtered_tickers(tickers)
 
 print(f"Filtered List({len(filtered)}):")
@@ -26,7 +37,7 @@ if first_mon < 1:
     first_mon = first_mon + max(cal.itermonthdays(current_year, current_month_num-1))
 
 
-file_path = f"weekly-csv/week_beginning_{num_to_month}_{str(first_mon)}.csv"
+file_path = f"weekly-csv/week_beginning_{num_to_month}_{str(first_mon)}.csv" # replace with absolute path for cron job
 filtered_with_count = [len(filtered)] + filtered
 
 with open(file_path, mode='a', newline='') as file:

d8c98e6baefc570dd59edb98751c772d519861fa
ukduw <edhkmayo@gmail.com>
2025-09-25 07:42:18 -0400

    added table fetch, row iterator, get text from % gain column, if >30, get ticker text from same row and append to list

--- fetch_afterhours.py
+++ fetch_afterhours.py
@@ -8,7 +8,19 @@ def get_afterhours_gainers_from_tradingview(filter_url):
     response = requests.get(filter_url, headers=headers)
     soup = BeautifulSoup(response.text, "html.parser")
 
+    table = soup.find_all("table")[1]
 
+    for row in enumerate(table.find_all("tr")):
+        gains = row.find_all("span", class_="positive-p_QIAEOQ")
+        for g in gains:
+            gain = g.get_text(strip=True)
+            gain_cleaned = gain[1:-1]
+
+            if int(gain_cleaned) > 30: # 30% gain, tweak later
+                ticker_containers = row.find_all("a")
+                for t in ticker_containers:
+                    ticker = t.get_text(strip=True)
+                    ticker.append(ticker)
 
     print(f"After-hours screen returned {len(tickers)} Tickers")
 

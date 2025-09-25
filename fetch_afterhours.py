2677ecb417ffdd8945d876c3cc61cd4d243b3b91
ukduw <edhkmayo@gmail.com>
2025-09-25 07:19:49 -0400

    added skeleton function for fetching after-hours gainers from tradingview

---
+++ fetch_afterhours.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_afterhours_gainers_from_tradingview(filter_url):
+    tickers = []
+    headers = {'User-Agent': "Mozilla/5.0"}
+
+    response = requests.get(filter_url, headers=headers)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+
+
+    print(f"After-hours screen returned {len(tickers)} Tickers")
+
+    
+    return tickers

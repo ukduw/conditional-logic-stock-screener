41fd9c25417a5897cbd3166ae4cb52e1a4535961
ukduw <edhkmayo@gmail.com>
2026-07-05 12:44:36 +0100

    added etf/ dir and placeholder modules

---
+++ etf/run.py
@@ -0,0 +1,53 @@
+from stock.fetch_tickers import get_all_finviz_tickers
+from stock.fetch_tickerview import get_finviz_tickers_from_tickerview
+from stock.fetch_afterhours import get_afterhours_gainers_from_tradingview
+from stock.screener import filtered_tickers
+import csv
+import datetime, calendar
+
+base_url = "https://finviz.com/screener.ashx?v=411&f=cap_smallunder%2Cgeo_usa%2Csh_curvol_o1000%2Csh_price_u20%2Csh_relvol_o3&o=-change"
+    # initial screener
+    # https://finviz.com/screener.ashx?v=111&f=cap_smallunder%2Cgeo_usa%2Csh_curvol_o1000%2Csh_price_u20%2Csh_relvol_o2&o=-change
+    # initial screener + 3x r.vol, TICKER ONLY
+    # https://finviz.com/screener.ashx?v=411&f=cap_smallunder%2Cgeo_usa%2Csh_curvol_o1000%2Csh_price_u20%2Csh_relvol_o3&o=-change
+base_url2 = "https://www.tradingview.com/markets/stocks-usa/market-movers-after-hours-gainers"
+
+# tickers = get_all_finviz_tickers(base_url)
+tickers = get_finviz_tickers_from_tickerview(base_url)
+filtered = filtered_tickers(tickers)
+afters = get_afterhours_gainers_from_tradingview(base_url2)
+
+
+current_date = datetime.datetime.now()
+
+rounded_sec = (current_date - datetime.timedelta(microseconds=current_date.microsecond // 1000 * 1000))
+without_milli = rounded_sec.replace(microsecond=0)
+
+print(f"=== {without_milli} ===")
+print(f"Filtered List({len(filtered)}):")
+print(filtered)
+print(f"After-hours Gainers({len(afters)}):")
+print(afters, "\n")
+
+
+current_year = int(current_date.strftime("%Y")) # 2025...
+current_month_num = int(current_date.strftime("%m")) # 01-12
+current_day_num = int(current_date.strftime("%d")) # 01-31
+current_weekday = current_date.weekday() # 0-6 - 0 is Monday
+
+first_mon = current_day_num - current_weekday
+num_to_month = calendar.month_abbr[current_month_num]
+cal = calendar.Calendar()
+if first_mon < 1:
+    num_to_month = calendar.month_abbr[current_month_num - 1]
+    first_mon = first_mon + max(cal.itermonthdays(current_year, current_month_num-1))
+
+
+file_path = f"/home/edhkm/edliu/conditional-logic-stock-screener/weekly-csv/{current_year}/week_beginning_{num_to_month}_{first_mon}.csv"
+filtered_with_count = [len(filtered)] + filtered
+afters_with_count = [len(afters)] + afters
+
+with open(file_path, mode='a', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(filtered_with_count + [" - "] + afters_with_count)
+    file.write("\n\n")
---
+++ etf/screener.py
@@ -0,0 +1,22 @@
+import yfinance as yf
+
+def passes_custom_filter(ticker):
+    try:
+        info = yf.Ticker(ticker).info
+        volume = info.get("volume")
+        float_shares = info.get("floatShares")
+        dollar_volume = info.get("currentPrice") * volume # regularMarketPrice?
+        market_cap = info.get("marketCap")
+
+        # 1) Float rotation check, 2) $Vol > MCap", 3) 
+        return (volume > float_shares and dollar_volume > market_cap) if float_shares is not None else (dollar_volume > market_cap)
+    except Exception as e:
+        print(f"Error for {ticker}: {e}")
+        return False
+
+def filtered_tickers(ticker_list):
+    return [t for t in ticker_list if passes_custom_filter(t)]
+
+
+# note: no otc coverage on finviz, but alpaca can't trade otc's anyways...
+    # ibkr can - for now, just keep an eye on otcs and continue testing...

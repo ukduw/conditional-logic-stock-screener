14c3ecf0c265e68f399e26e10e6e4a231037eaad
ukduw <edhkmayo@gmail.com>
2026-07-05 17:34:39 +0100

    orchestrates funcs and saves output to json + log prints... will update file_path after testing

--- etf/run.py
+++ etf/run.py
@@ -1,21 +1,16 @@
-from stock.fetch_tickers import get_all_finviz_tickers
-from stock.fetch_tickerview import get_finviz_tickers_from_tickerview
-from stock.fetch_afterhours import get_afterhours_gainers_from_tradingview
-from stock.screener import filtered_tickers
-import csv
-import datetime, calendar
+from etf.fetch_etfs import get_etfs_from_etfcom_cache
+from etf.enrich import enrich_with_yf_data, sort_by_volatility
+import json
+import datetime
 
-base_url = "https://finviz.com/screener.ashx?v=411&f=cap_smallunder%2Cgeo_usa%2Csh_curvol_o1000%2Csh_price_u20%2Csh_relvol_o3&o=-change"
-    # initial screener
-    # https://finviz.com/screener.ashx?v=111&f=cap_smallunder%2Cgeo_usa%2Csh_curvol_o1000%2Csh_price_u20%2Csh_relvol_o2&o=-change
-    # initial screener + 3x r.vol, TICKER ONLY
-    # https://finviz.com/screener.ashx?v=411&f=cap_smallunder%2Cgeo_usa%2Csh_curvol_o1000%2Csh_price_u20%2Csh_relvol_o3&o=-change
-base_url2 = "https://www.tradingview.com/markets/stocks-usa/market-movers-after-hours-gainers"
 
-# tickers = get_all_finviz_tickers(base_url)
-tickers = get_finviz_tickers_from_tickerview(base_url)
-filtered = filtered_tickers(tickers)
-afters = get_afterhours_gainers_from_tradingview(base_url2)
+tickers = get_etfs_from_etfcom_cache()
+enriched = enrich_with_yf_data(tickers)
+sorted_list = sort_by_volatility(enriched)
+
+file_path = "etf_volatility.json"
+with open(file_path, "w") as f:
+    json.dump(sorted_list, f)
 
 
 current_date = datetime.datetime.now()
@@ -23,31 +18,8 @@ current_date = datetime.datetime.now()
 rounded_sec = (current_date - datetime.timedelta(microseconds=current_date.microsecond // 1000 * 1000))
 without_milli = rounded_sec.replace(microsecond=0)
 
+# FOR LOGS
 print(f"=== {without_milli} ===")
-print(f"Filtered List({len(filtered)}):")
-print(filtered)
-print(f"After-hours Gainers({len(afters)}):")
-print(afters, "\n")
+print(f"Filtered List({len(sorted_list)}):")
+print(sorted_list)
 
-
-current_year = int(current_date.strftime("%Y")) # 2025...
-current_month_num = int(current_date.strftime("%m")) # 01-12
-current_day_num = int(current_date.strftime("%d")) # 01-31
-current_weekday = current_date.weekday() # 0-6 - 0 is Monday
-
-first_mon = current_day_num - current_weekday
-num_to_month = calendar.month_abbr[current_month_num]
-cal = calendar.Calendar()
-if first_mon < 1:
-    num_to_month = calendar.month_abbr[current_month_num - 1]
-    first_mon = first_mon + max(cal.itermonthdays(current_year, current_month_num-1))
-
-
-file_path = f"/home/edhkm/edliu/conditional-logic-stock-screener/weekly-csv/{current_year}/week_beginning_{num_to_month}_{first_mon}.csv"
-filtered_with_count = [len(filtered)] + filtered
-afters_with_count = [len(afters)] + afters
-
-with open(file_path, mode='a', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(filtered_with_count + [" - "] + afters_with_count)
-    file.write("\n\n")
